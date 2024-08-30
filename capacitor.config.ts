0482704fc5e318849775a7b7ac846e016b99c784
Nico <nicola.zorzo@gmail.com>
2024-08-30 15:14:09 +0200

    add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vault',
+  appName: 'vault',
+  webDir: 'dist/vault/browser'
+};
+
+export default config;
