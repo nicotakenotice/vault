b8505acb1d801d9bc4e34f4310684501619774ba
Nico <nicola.zorzo@gmail.com>
2024-08-30 16:01:32 +0200

    change default theme, test ionic components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StatusBar, Style } from '@capacitor/status-bar';
+import {
+  IonApp,
+  IonContent,
+  IonFooter,
+  IonHeader,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IonApp, IonContent, IonFooter, IonHeader, IonTitle, IonToolbar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'vault';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.setStatusBarStyleLight();
+  }
+
+  setStatusBarStyleLight = async () => {
+    await StatusBar.setStyle({ style: Style.Light });
+  };
 }
