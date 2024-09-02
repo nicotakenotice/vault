13fdb645c731b63c82eb903d08e91dbddbe97770
Nico <nicola.zorzo@gmail.com>
2024-09-02 23:40:23 +0200

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './routes/home/home.component';
+import { VaultComponent } from './routes/vault/vault.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: ':owner',
+    component: VaultComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/'
+  }
+];
