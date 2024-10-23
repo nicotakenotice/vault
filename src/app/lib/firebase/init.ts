230b88f2f8d7fa8a8f465b06647ba4a39db98eee
Nico <nicola.zorzo@gmail.com>
2024-10-23 22:54:13 +0200

    add firestore

--- src/app/lib/firebase/init.ts
+++ src/app/lib/firebase/init.ts
@@ -1,4 +1,6 @@
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 import { firebaseConfig } from './config';
 
 export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
